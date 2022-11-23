0aa6c83057853173a4dace2f9c67d1f4bc55d979
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-11-23 10:49:04 +0530

    Day & Month Command Line

--- ifelse.sh
+++ ifelse.sh
@@ -1,39 +1,10 @@
-n1=$((RANDOM%1000));
-n2=$((RANDOM%1000));
-n3=$((RANDOM%1000));
-n4=$((RANDOM%1000));
-n5=$((RANDOM%1000));
+read -p " Enter Date:-" date
+read -p " Enter Month:-" Month
 
-if [ $n1 -gt $n2 ] && [ $n1 -gt -$n3 ] && [ $n1 -gt $n4 ] && [ $n1 -gt $n5 ];
+if (( ($Month <= 6 && $date <= 20) && (($Month >= 3 && $date <= 20) && ($date<31)) ))
 then
-echo "Given Number is maximum:" $n1
-elif [ $n2 -gt $n1 ] && [ $n2 -gt -$n3 ] && [ $n2 -gt $n4 ] && [ $n2 -gt $n5 ];
-then
-echo "Given Number is Maximum:" $n2
-elif [ $n3 -gt $n1 ] && [ $n3 -gt -$n2 ] && [ $n3 -gt $n4 ] && [ $n3 -gt $n5 ];
-then
-echo "Given Number is Maximum:" $n3
-elif [ $n4 -gt $n1 ] && [ $n4 -gt -$n2 ] && [ $n4 -gt $n3 ] && [ $n4 -gt $n5 ];
-then
-echo "Given Number is Maximum:" $n4
+        echo $Month $date "True";
 else
-echo "Given no is Maximum:" $n5
+
+        echo "False";
 fi
-
-if [ $n1 -lt $n2 ] && [ $n1 -lt -$n3 ] && [ $n1 -lt $n4 ] && [ $n1 -lt $n5 ];
-then
-echo "Given Number is minimum:" $n1
-elif [ $n2 -lt $n1 ] && [ $n2 -lt -$n3 ] && [ $n2 -lt $n4 ] && [ $n2 -lt $n5 ];
-then
-echo "Given Number is minimum:" $n2
-elif [ $n3 -lt $n1 ] && [ $n3 -lt -$n2 ] && [ $n3 -lt $n4 ] && [ $n3 -lt $n5 ];
-then
-echo "Given Number is minimum:" $n3
-elif [ $n4 -lt $n1 ] && [ $n4 -lt -$n2 ] && [ $n4 -lt $n3 ] && [ $n4 -lt $n5 ];
-then
-echo "Given Number is Minimum:" $n4
-else
-echo "Given no is minimum:" $n5
-fi
-
-
